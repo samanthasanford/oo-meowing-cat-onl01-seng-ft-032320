8673c578d32e5e134322bfea4fef02ed0ee58caa
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-01 01:06:39 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_reader :name
+  attr_writer :meow
+
+  def meow
+    puts "meow!"
+  end
+end
+
+maru.new
+maru.name = "Maru"
+maru.meow
