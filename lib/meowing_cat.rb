b568084ee6223a06ef14cc45edbc122cdfc4d94b
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-01 01:09:01 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_accessor :name
+  attr_writer :meow
+
+  def meow
+    puts "meow!"
+  end
+end
+
+maru = Cat.new
+maru.name = "Maru"
+maru.name
+maru.meow
